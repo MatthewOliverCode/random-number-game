4057072591b6a69a3c3030d2836278d5b83d7d4f
Matthew Oliver <>
2021-04-04 16:52:42 +0000

    [+] Added if/else statement

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import random
-user_guess = input("Guess a number between 1 and 10 \n —>")
+user_guess = input("Guess a number between 1 and 10 (Inclusive) \n —>")
 random_number = random.randint(1,10)
 user_score = 0
 
 # MAIN PROGRAM
+def game():
+  if random_number == user_guess:
+    user_score += 1
+    print(f"You guessed correctly! 😀 Your score is currently {user_score}")
+  else:
+    print("Unlucky")
