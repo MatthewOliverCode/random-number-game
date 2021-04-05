eeb395940f8efbea903aa7fe43305af5b1512f77
Matthew Oliver <>
2021-04-05 10:21:20 +0000

    [+] Amended game() so that guesses are given 5 times [+] Casted rand_num to string [-] Deleted the display of the randomly generated number

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import random
-user_guess = input("Guess a number between 1 and 10 (Inclusive) \n\t—> ")
-random_number = random.randint(1,10)
+
+
 user_score = 0
 
 # MAIN PROGRAM
-def game(rand_num,user_guess,user_score):
-  if rand_num == user_guess:
-    user_score += 1
-    print(f"You guessed correctly! :D Your score is currently {user_score}")
-  else:
-    print(f"Unlucky. The randomly generated number was {rand_num}")
-game(random_number,user_guess,user_score)
+def game(user_score):
+  rand_num = str(random.randint(1,10))
+
+  for _ in range(0,5):
+    user_guess = input("Guess a number between 1 and 10 (Inclusive) \n\t—> ")
+
+    if rand_num == user_guess:
+      user_score += 1
+      print(f"You guessed correctly! :D Your score is currently {user_score}")
+    else:
+      print("Unlucky")
+game(user_score)
