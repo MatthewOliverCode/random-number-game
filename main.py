5eab5ee965c10771ad3424bdeae636f9e1842991
Matthew Oliver <>
2021-04-05 08:35:30 +0000

    [+] Added parameters and arguments [-] Deleted emoji

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ random_number = random.randint(1,10)
 user_score = 0
 
 # MAIN PROGRAM
-def game():
-  if random_number == user_guess:
+def game(rand_num,user_guess,user_score):
+  if rand_num == user_guess:
     user_score += 1
-    print(f"You guessed correctly! 😀 Your score is currently {user_score}")
+    print(f"You guessed correctly! :D Your score is currently {user_score}")
   else:
     print("Unlucky")
+game(random_number,user_guess,user_score)
