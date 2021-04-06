6e563d4ca2b6f63a5f70379b6a05aaf84112e11c
Matthew Oliver <>
2021-04-06 19:49:38 +0000

    [+] Allows user to specify number of questions produced [+] Display score out of number of questions

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import random
 user_score = 0
 lower_bound = int(input("Enter the lowest number that you want to guess \n\t—> "))
 upper_bound = int(input("Enter the highest number that you want to guess \n\t—> "))
+question_amount = int(input("How many questions would you like to do? \n\t—> "))
 
 # MAIN PROGRAM
-def game(user_score,lower,upper):
-  
-  for _ in range(0,5):
+def game(user_score,lower,upper,num_questions):
+
+  for _ in range(num_questions):
     rand_num = str(random.randint(lower,upper))
     user_guess = input(f"Guess a number between {lower} and {upper} \n\t—> ")
 
@@ -15,5 +16,6 @@ def game(user_score,lower,upper):
       print(f"You guessed correctly! :D Your score is currently {user_score}")
     else:
       print("Unlucky")
-  print(f"Your score was {user_score}")
-game(user_score,lower_bound,upper_bound)
+  print(f"Your score is {user_score} / {num_questions}")
+
+game(user_score,lower_bound,upper_bound,question_amount)
