26e728feba1907a50607e9c044e60ccad785c4b5
Matthew Oliver <>
2021-04-06 20:10:56 +0000

    [+] Implementing VividHues and using its formatting to improve the UI [+] Display boundaries when asking user to guess a number

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import random
+from VividHues import Clr
 user_score = 0
-lower_bound = int(input("Enter the lowest number that you want to guess \n\t—> "))
-upper_bound = int(input("Enter the highest number that you want to guess \n\t—> "))
-question_amount = int(input("How many questions would you like to do? \n\t—> "))
+lower_bound = int(input(f"Enter the {Clr.BOLD}lowest{Clr.RESET} number that you want to guess \n\t—> "))
+upper_bound = int(input(f"Enter the {Clr.BOLD}highest{Clr.RESET} number that you want to guess \n\t—> "))
+question_amount = int(input(f"How many {Clr.BOLD}questions{Clr.RESET} would you like to do? \n\t—> "))
 
 # MAIN PROGRAM
 def game(user_score,lower,upper,num_questions):
 
   for _ in range(num_questions):
     rand_num = str(random.randint(lower,upper))
-    user_guess = input(f"Guess a number between {lower} and {upper} \n\t—> ")
+    user_guess = input(f"Guess a number between {Clr.BOLD}{lower}{Clr.RESET} and {Clr.BOLD}{upper}{Clr.RESET} \n\t—> ")
 
     if rand_num == user_guess:
       user_score += 1
-      print(f"You guessed correctly! :D Your score is currently {user_score}")
+      print(f"You guessed correctly! :D Your score is currently {Clr.BOLD}{user_score}{Clr.RESET}")
     else:
       print("Unlucky")
-  print(f"Your score is {user_score} / {num_questions}")
+  print(f"Your score is {Clr.BOLD}{user_score}{Clr.RESET} / {Clr.BOLD}{num_questions}")
 
 game(user_score,lower_bound,upper_bound,question_amount)
